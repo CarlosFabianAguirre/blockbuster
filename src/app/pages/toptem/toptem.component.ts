ce2a631c818a26969cf86b7a4bc38d3ce0e6f736
CarlosFabianAguirre <carlosfabianaguirre@hotmail.com>
2022-08-03 18:34:53 -0300

    hasta ahora hecho fondo lindo y componentes por arriba

--- src/app/pages/toptem/toptem.component.ts
+++ src/app/pages/toptem/toptem.component.ts
@@ -11,5 +11,51 @@ export class ToptemComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  imgCollection: Array<object> = [
+    {
+      image: '/assets/rock/misfits.jpg',
+      thumbImage: '/assets/rock/misfits.jpg',
+      alt: 'imagen Misfits',
+      title: 'Misfits',
+    }, {
+      image: '/assets/rock/motorhead.jpg',
+      thumbImage: "/assets/rock/motorhead.jpg",
+      title: 'Motorhead',
+      alt: 'imagen Motorhead'
+    }, {
+      image: '/assets/rock/davidbowie.jpg',
+      thumbImage: '/assets/rock/davidbowie.jpg',
+      title: 'Davidbowie',
+      alt: 'imagen davidbowie'
+    }, {
+      image: '/assets/rock/ramones.jpg',
+      thumbImage: '/assets/rock/ramones.jpg',
+      title: 'Ramones',
+      alt: 'Imagen ramones'
+    }, {
+      image: '/assets/rock/pulsion.jpg',
+      thumbImage: '/assets/rock/pulsion.jpg',
+      title: 'Pulsion',
+      alt: 'Image 5'
+    }, {
+      image: '/assets/rock/theclash.jpg',
+      thumbImage: '/assets/rock/theclash.jpg',
+      title: 'The Clash',
+      alt: 'imagen the clash'
+    },  {
+      image: '/assets/rock/michael.jpg',
+      thumbImage: '/assets/rock/michael.jpg',
+      title: 'Michael Jackson',
+      alt: 'Imagen Michael Jackson',
+      
+    },  {
+      image: '/assets/rock/michael.jpg',
+      thumbImage: '/assets/rock/kiss.jpg',
+      title: 'Kiss',
+      alt: 'Imagen Kiss'
+    }
+];
 
 }
+
+
